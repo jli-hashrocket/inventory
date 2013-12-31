f758d516d1e97981de75662c091e234c5dde007b
macheteshuffle <jeff.li029@gmail.com>
2013-12-30 19:04:20 -0500

    Created rspec test for Inventory

--- spec/models/inventory_spec.rb
+++ spec/models/inventory_spec.rb
@@ -4,10 +4,10 @@ describe Inventory do
   it { should have_valid(:title).when('Beef') }
   it { should_not have_valid(:title).when(nil) }
 
-  # it { should have_valid(:description).when('Tons of it!') }
-  # it { should_not have_valid(:description).when(nil) }
+  it { should have_valid(:description).when('Tons of it!') }
+  it { should_not have_valid(:description).when(nil) }
 
-  # it { should have_valid(:quantity).when(3) }
-  # it { should_not have_valid(:quantity).when(nil) }
+  it { should have_valid(:quantity).when(3) }
+  it { should_not have_valid(:quantity).when(nil) }
 
 end
